7cc3cc58af95f6a2c8bbb3520bfdaec6bbb77e39
yoshonabee <jhann0120@gmail.com>
2020-05-14 14:12:14 +0800

    <chores>: change audio sample rate to 8000Hz

--- scripts/preprocess_musdb18.sh
+++ scripts/preprocess_musdb18.sh
@@ -1,2 +1,2 @@
-python3.6 src/utils/stem2wav.py ../data/musdb18 data/musdb18_separated
+python3.6 src/utils/stem2wav.py ../data/musdb18 data/musdb18_separated --sample_rate 8000
 python3.6 src/utils/make_input_json.py data/musdb18_separated .
