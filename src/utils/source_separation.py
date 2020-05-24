bb7dfaa3fbf3e874e67af30329976d2c7df12f2c
yoshonabee <jhann0120@gmail.com>
2020-05-24 15:16:08 +0800

    <fix>: fix bugs for source separation script

--- src/utils/source_separation.py
+++ src/utils/source_separation.py
@@ -1,11 +1,12 @@
-import subprocesses
+import subprocess
 from multiprocessing import Pool
 from pathlib import Path
 from argparse import ArgumentParser
 
 from tqdm import tqdm
 
-def get_args(args):
+def get_args():
+    parser = ArgumentParser()
     parser.add_argument("data_dir")
     parser.add_argument("output_dir")
 
@@ -15,16 +16,14 @@ def main(args):
     data_dir = Path(args.data_dir)
     output_dir = Path(args.output_dir)
 
-    assert data_dir.isdir()
-
-    if not output_dir.exists() or not output_dir.isdir():
+    if not output_dir.exists() or not output_dir.is_dir():
         output_dir.mkdir(parents=True, exist_ok=True)
 
     for audio in data_dir.glob("*.wav"):
-        subprocesses.run("spleeter", "separate", "-i", str(audio), "-p", "spleeter:2stems", "-o", args.output_dir)
+        subprocess.run(["spleeter", "separate", "-i", str(audio), "-p", "spleeter:2stems", "-o", args.output_dir])
 
 if __name__ == '__main__':
-    args = get_args(args)
+    args = get_args()
     print(args)
 
     main(args)
