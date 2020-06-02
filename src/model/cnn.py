fec59713dd61267a6ae09397522bf72eda0658d4
yoshonabee <jhann0120@gmail.com>
2020-06-02 19:03:22 +0800

    residual cnn model

--- src/model/cnn.py
+++ src/model/cnn.py
@@ -16,31 +16,40 @@ class CNN(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d((3, 1)),
             nn.Dropout(0.3),
-            nn.Conv2d(20, 60, (3, 3), 1, (1, 1)),
-            nn.ReLU(),
-            nn.BatchNorm2d(60),
-            nn.Conv2d(60, 60, (3, 3), 1, (1, 1)),
-            nn.ReLU(),
-            nn.BatchNorm2d(60),
-            nn.Conv2d(60, 60, (3, 3), 1, (1, 1)),
+            nn.Conv2d(20, 60, (3, 3), 1, (1, 1))
+        ]
+
+        self.cnns = nn.ModuleList(
+            [
+                nn.Sequential(
+                    nn.ReLU(),
+                    nn.BatchNorm2d(60)
+                    nn.Conv2d(60, 60, (3, 3), 1, (1, 1)),
+                )
+                for i in range(5)
+            ]
+        )
+
+        self.before_out = nn.Sequential(
             nn.ReLU(),
             nn.BatchNorm2d(60),
             nn.Dropout(0.3)
-        ]
+        )
 
         self.net = nn.Sequential(*layers)
 
-        layers = [
+        self.out = nn.Sequential(
             nn.Linear(3360, 256),
             nn.ReLU(),
             nn.Linear(256, 1),
             nn.Sigmoid()
-        ]
-
-        self.out = nn.Sequential(*layers)
+        )
     
     def forward(self, x):
         x = x.unsqueeze(1)
         x = self.net(x)
+        for layer in self.cnns:
+            x = x + layer(x)
+        x = self.before_out(x)
         x = x.view(x.size(0), -1)
         return self.out(x)
