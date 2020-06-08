c4da5118606f0e9dc4e0180b26a0026f5208d23e
yoshonabee <jhann0120@gmail.com>
2020-06-08 15:55:40 +0800

    add small model5

--- src/train.sh
+++ src/train.sh
@@ -1,7 +1,7 @@
 python3 train.py \
     train.json val.json \
-    --model_config config/model5.json \
-    --weight_decay 0.001 \
+    --model_config config/small_model5.json \
+    --weight_decay 0.0002 \
     --dropout 0 \
     --criterion bceloss \
-    --name cnn_rnn3_model5_bidirectional
+    --name cnn_rnn3_small_model5_bidirectional
