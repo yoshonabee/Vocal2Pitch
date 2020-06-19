5f7a5e44e1b403ea1d0171a317e0062fffba2ae2
yoshonabee <jhann0120@gmail.com>
2020-06-19 16:42:59 +0800

    end-to-end

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -12,7 +12,7 @@ import torch
 from tqdm import tqdm
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, data_json, thres, target_length, segment_length=4, sr=16000, data_amount=5, augment=True):
+    def __init__(self, data_json, thres, target_length, segment_length=4, sr=16000, data_amount=1, augment=True):
         self.data_json = Path(data_json)
         self.thres = thres
         self.target_length = target_length
@@ -30,10 +30,11 @@ class Dataset(torch.utils.data.Dataset):
     def load_data(self):
         self.data = []
         self.target = []
+        self.pitch = []
 
         with tqdm(json.load(open(self.data_json)), unit="audio") as t:
             t.set_description("Loading audios")
-
+            j = 0
             for audio_dir in t:
                 audio_dir = Path(audio_dir)
                 audio_path = audio_dir / f"vocals-16k.wav"
@@ -43,7 +44,7 @@ class Dataset(torch.utils.data.Dataset):
                 
                 label = pd.read_csv(label_path, sep=" ", header=None, names=["start", "end", "pitch"])
                 onset_list = get_onset_list(label, self.thres)
-
+                
                 onset_idx = 0
                 for i, frame in enumerate(range(0, audio.shape[0] - self.segment_frame + 1, self.segment_frame)):
                     start_time = i * self.segment_length
@@ -51,22 +52,26 @@ class Dataset(torch.utils.data.Dataset):
 
                     onsets = []
                     pitchs = [onset_list[onset_idx - 1]] if onset_idx > 0 else []
-                    while onset_idx < len(onset_list) and onset_list[onset_idx] < end_time:
+                    
+                    while onset_idx < len(onset_list) and onset_list[onset_idx][0] < end_time:
                         onsets.append(onset_list[onset_idx][0])
                         pitchs.append(onset_list[onset_idx])
                         onset_idx += 1
-
+                    
                     target = make_target_tensor(onsets, start_time, self.segment_length, self.target_length)
                     pitch = make_pitch_tensor(pitchs, start_time, self.segment_length, self.target_length)
                     
-                    self.data.append([audio[frame:frame + self.segment_frame], len(self.target), len(self.pitch)])
+                    self.data.append([audio[frame:frame + self.segment_frame], len(self.target), [len(self.pitch), 0]])
                     self.target.append(target)
                     self.pitch.append(pitch)
-
+                #j += 1
+                #if j ==5 :
+                #    break
     def __getitem__(self, index):
-        audio, target, pitch = self.data[index]
+        audio, target, (pitch, pitch_factor) = self.data[index]
         target = self.target[target]
-        pitch = self.pitch[pitch]
+        pitch = self.pitch[pitch] + pitch_factor
+        
 
         return torch.tensor(audio).float(), target.float(), pitch.float()
 
@@ -80,26 +85,29 @@ class Dataset(torch.utils.data.Dataset):
             t.set_description("Augmentating audios")
 
             with Pool(4) as p:
-                for results, targets in p.imap(_augmentation, data):
-                    self.data.extend(list(zip(results, targets)))
+                for results, targets, pitches in p.imap(_augmentation, data):
+                    self.data.extend(list(zip(results, targets, pitches)))
                     t.update(1)
 
     def __len__(self):
         return len(self.data)
 
 def _augmentation(inputs):
-    (audio, target), data_amount, sr = inputs
+    (audio, target, pitch), data_amount, sr = inputs
 
     results = []
+    pitches = []
     for i in range(data_amount - 1):
-        pitch_factor = random.randint(-12, 12)
+        pitch_factor = random.randint(-5, 5)
         noise_factor = 9 * random.random() / 100 + 1e-3 # [1e-3, 1e-2]
+        
+        pitches.append([pitch[0], pitch_factor])
         # noise
         noise = np.random.randn(audio.shape[0]) * noise_factor
         augmented = librosa.effects.pitch_shift(audio, sr, pitch_factor) + noise
         results.append(augmented)
 
-    return results, [target for _ in range(len(results))]
+    return results, [target for _ in range(len(results))], pitches
 
 
 
--- src/data/utils.py
+++ src/data/utils.py
@@ -1,7 +1,8 @@
+import numpy as np
 import torch
 
 def get_onset_list(df, thres):
-    result = [df['start'][0]]
+    result = [[df['start'][0], df['pitch'][0]]]
 
     for i in range(1, df.shape[0]):
         if df['start'][i] - df['end'][i - 1] > thres:
@@ -42,24 +43,27 @@ def make_target_tensor(onset_list, start_time, time_length, length):
     return tensor
 
 def make_pitch_tensor(pitch_list, start_time, time_length, length):
-    time_per_frame = time_length / length
+    try:
+        time_per_frame = time_length / length
+   
+        tensor = []
+        onset_idx = 0
 
-    tensor = []
-    onset_idx = 0
+        if pitch_list[0][0] < start_time:
+            while (len(tensor) + 1) * time_per_frame + start_time < pitch_list[1][0]:
+                tensor.append(pitch_list[0][1])
 
-    if pitch_list[0][0] < start_time:
-        while (len(tensor) + 1) * time_per_frame + start_time < pitch_list[1][0]:
-            tensor.append(pitch_list[0][1])
+            onset_idx = 1
 
-        onset_idx = 1
+        for i in range(len(tensor), length):
+            if onset_idx + 1 < len(pitch_list):
+                t = (i + 1) * time_per_frame + start_time
 
-    for i in range(len(tensor), length):
-        if onset_idx + 1 < len(pitch_list):
-            t = (i + 1) * time_per_frame + start_time
+                if t >= pitch_list[onset_idx + 1][0]:
+                    onset_idx += 1
 
-            if t >= pitch_list[onset_idx + 1][0]:
-                onset_idx += 1
+            tensor.append(pitch_list[onset_idx][1])
 
-        tensor.append(pitch_list[onset_idx][1])
-
-    return torch.tensor(tensor).float()
+        return torch.tensor(tensor).float()
+    except:
+        return torch.zeros(length).float()
