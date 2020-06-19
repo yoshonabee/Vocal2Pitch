2154015b3365930d3ee4d5cd838a98b74d81d995
yoshonabee <jhann0120@gmail.com>
2020-06-19 16:48:41 +0800

    big kernel size and stride 16,16,2

--- src/train.sh
+++ src/train.sh
@@ -1,10 +1,9 @@
 python3 train.py \
     train.json val.json \
-    --model_config config/16ms_model5.json \
+    --model_config config/model5.json \
     --lr 0.0001 \
     --epochs 150 \
     --dropout 0 \
     --criterion bceloss \
     --batch_size 256 \
-    --inbalance_ratio 5 \
-    --name 16ms_resample5_fix_dataAug5_cnn_16msModel5_transformer_WD0_LR1e-4
+    --name big_kernel_size_stride_16_16_2
